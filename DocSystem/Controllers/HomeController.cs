7a81a088e3bbac9dec1ab49d0624ac888c443de6
kamila-ropela <kamila.ropela@gmail.com>
2019-03-20 12:56:03 +0100

    Init project

---
+++ DocSystem/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DocSystem.Models;
+
+namespace DocSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DocSystem/Controllers/LabController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DocSystem.Controllers
+{
+    public class LabController : Controller
+    {
+    }
+}
