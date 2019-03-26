3d2b4c1e8f9b64d1a71497d605a0f4818aa6db66
mallop <malopatowska@gmail.com>
2019-03-26 19:29:50 +0100

    Add files via upload

--- DocSystem/Controllers/PatientController.cs
+++ DocSystem/Controllers/PatientController.cs
@@ -1,8 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
-namespace DocSystem.Controllers
+namespace Patients.Controllers
 {
     public class PatientController : Controller
     {
+        
+        public ActionResult PatientView()
+        {
+            return View();
+        }
     }
 }
