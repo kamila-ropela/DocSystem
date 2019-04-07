54ad970db1e1c520dfe97d3fa57bf72792e96871
kamila-ropela <kamila.ropela@gmail.com>
2019-04-07 18:24:14 +0200

    Added LogIn view

--- DocSystem/Models/User.cs
+++ DocSystem/Models/User.cs
@@ -1,11 +1,14 @@
-﻿namespace DocSystem.Models
+﻿using System.ComponentModel;
+
+namespace DocSystem.Models
 {
     public class User
     {
+        [DisplayName("Login")]
         public string Login { get; set; }
-
+        [DisplayName("Haslo")]
         public string Password { get; set; }
-
+        [DisplayName("Rola")]
         public string Role { get; set; }
     }
 }
