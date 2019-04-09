6cdd1c99b646d1c304f1ecb9656f367701922c7e
kamila-ropela <kamila.ropela@gmail.com>
2019-04-09 22:06:16 +0200

    Revert "Revert "Revert "Change model name"""
    This reverts commit be43fedfa5cf1b8e52f1e8007fe314602c4d4bed.

--- DocSystem/Models/Documentation.cs
+++ DocSystem/Models/Documentation.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace DocSystem.Models
@@ -7,14 +6,14 @@ namespace DocSystem.Models
     public class Documentation
     {
         public int Id { get; set; }
-        [DisplayName("Id pacjenta")]
+
         public int PatientId { get; set; }
-        [DisplayName("Id doktora")]
+
         public int DoctorId { get; set; }
+
         [Required(ErrorMessage = "This field is required")]
-        [DisplayName("Choroba")]
         public string Disease { get; set; }
-        [DisplayName("Data")]
+
         public DateTime Date { get; set; }
     }
 }
--- DocSystem/Models/Visit.cs
+++ DocSystem/Models/Visit.cs
@@ -1,22 +1,21 @@
 ﻿using System;
-using System.ComponentModel;
 
 namespace DocSystem.Models
 {
     public class Visit
     {
         public int Id { get; set; }
-        [DisplayName("Id pacjenta")]
+
         public int PatientId { get; set; }
-        [DisplayName("Id doktora")]
+
         public int DoctorId { get; set; }
-        [DisplayName("Typ")]
+
         public string Type { get; set; }
-        [DisplayName("Doktor")]
+
         public string Doctor { get; set; }
-        [DisplayName("Status")]
+
         public string Status { get; set; }
-        [DisplayName("Data")]
+
         public DateTime Date { get; set; }
     }
 }
